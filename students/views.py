a6bc44195480d37cabcd7a53336795aa7f49a44d
CHETAN S/IHQ/TECH/TS <124719@ecomexpress.in>
2025-08-04 12:25:10 +0530

    Initial commit

---
+++ students/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from rest_framework import viewsets,filters
+from .models import Student
+from .serializers import StudentSerializer
+
+class StudentViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['first_name', 'last_name']
+
+
+
